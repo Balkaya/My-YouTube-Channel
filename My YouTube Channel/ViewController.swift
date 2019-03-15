00706f0ac0c526f5943379c80e8657fce45ddc8e
Can <kralciplak9@gmail.com>
2019-03-15 19:23:24 +0300

    Initial Commit

---
+++ My YouTube Channel/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  My YouTube Channel
+//
+//  Created by Can Balkaya on 3/15/19.
+//  Copyright © 2019 Can Balkaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
